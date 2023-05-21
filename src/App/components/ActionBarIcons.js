a6ee1e4830eea69d2875a50b67c445ef1435fe8d
dhruvd-grappus <dhruv.devasthale@grappus.com>
2023-05-21 17:34:55 +0530

    add on tap

--- src/App/components/ActionBarIcons.js
+++ src/App/components/ActionBarIcons.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function ActionBarIcons({ path, alt }) {
+export default function ActionBarIcons({ path, alt, onTap }) {
   return (
-    <div className="action-icon rounded-full p-1 bg-orange-400">
+    <div className="action-icon rounded-full p-1 bg-orange-400" onClick={onTap}>
       <img className="w-[10px]" src={path} alt={alt} />
     </div>
   );
